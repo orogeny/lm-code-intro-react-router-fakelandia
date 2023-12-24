ad4f7b1fc5be4bf02e43ae4da3c86ba57ebaac52
Paul <paulwharin@gmail.com>
2023-12-24 19:12:48 +0000

    chore: install and configure testing environment

---
+++ client/vitest.setup.ts
@@ -0,0 +1,8 @@
+// import { afterEach } from "vitest";
+// import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+
+// Nolonger needed:
+//  https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#using-cleanup
+
+// afterEach(() => cleanup());
