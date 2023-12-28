a1fb72f80fc928afc4dadc190b77bb0922c1dcdb
Paul <paulwharin@gmail.com>
2023-12-28 14:35:52 +0000

    feat: implement barebones 'MisdemeanoursPage' component and add to Routes

---
+++ client/src/components/misdemeanours_page/misdemeanours_page.tsx
@@ -0,0 +1,15 @@
+import styles from "./misdemeanours_page.module.css";
+
+function MisdemeanoursPage() {
+  return (
+    <>
+      <main className={styles.page}>
+        <section>
+          <p>There are currently no Misdemeanours</p>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export { MisdemeanoursPage };
--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "../layouts/main_layout";
 import { DummyPage } from "../dummy_page/dummy_page";
 import { HomePage } from "../home_page/home_page";
+import { MisdemeanoursPage } from "../misdemeanours_page/misdemeanours_page";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="misdemeanours" element={<DummyPage />} />
+        <Route path="misdemeanours" element={<MisdemeanoursPage />} />
         <Route path="confession" element={<DummyPage />} />
       </Route>
     </Routes>
