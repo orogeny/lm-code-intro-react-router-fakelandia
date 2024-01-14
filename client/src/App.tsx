aaf4a4f95ab96b1435c3862e5dab8c57324ef901
Paul <paulwharin@gmail.com>
2024-01-14 06:14:46 +0000

    chore: remove tanstack react query related code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./components/router/router";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Router />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
--- client/src/components/misdemeanours_page/misdemeanours_page.tsx
+++ client/src/components/misdemeanours_page/misdemeanours_page.tsx
@@ -1,19 +1,11 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
 import styles from "./misdemeanours_page.module.css";
 import { fetchMisdemeanours } from "./fetch_misdemeanours";
 import { MisdemeanourTable } from "./misdemeanour_table";
 
 function MisdemeanoursPage() {
-  const { data } = useSuspenseQuery({
-    queryKey: ["misdemeanourse", "all"],
-    queryFn: () => fetchMisdemeanours(20),
-    gcTime: Infinity,
-    staleTime: Infinity,
-  });
-
   return (
     <main className={styles.page}>
-      <MisdemeanourTable items={data} />
+      <MisdemeanourTable items={[]} />
     </main>
   );
 }
