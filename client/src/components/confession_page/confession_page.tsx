2dbb3653087880a1d45d820b67667b005952fae9
Paul <paulwharin@gmail.com>
2024-01-15 17:03:29 +0000

    feat: implement saving new misdemeanour and error message display

--- client/src/components/confession_page/confession_page.tsx
+++ client/src/components/confession_page/confession_page.tsx
@@ -1,15 +1,63 @@
+import { useContext, useState } from "react";
+import { API_BASE_URL } from "../../environment_variables";
+import { MisdemeanoursContext } from "../../hooks/misdemeanour_context/misdemeanour_context";
 import { ConfessionForm, ConfessionFormData } from "./confession_form";
 import styles from "./confession_page.module.css";
+import {
+  MISDEMEANOURS,
+  Misdemeanour,
+  MisdemeanourKind,
+} from "../../misdemeanours.types";
+import { useNavigate } from "react-router-dom";
+
+type PostResponse = {
+  success: boolean;
+  justTalked: boolean;
+  message: string;
+};
 
 function ConfessionPage() {
-  const handleConfession = (form: ConfessionFormData) => {
-    console.log("Confession: ", form);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const context = useContext(MisdemeanoursContext);
+  const navigate = useNavigate();
 
-    // postConfession(form)
-    //   .then((response) => {
-    //     console.log("post response: ", response);
-    //   })
-    //   .catch((error) => console.log("post response error: ", error));
+  const handleConfession = (form: ConfessionFormData) => {
+    fetch(`${API_BASE_URL}/confess`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => {
+        if (!response.ok)
+          throw Error(`${response.status} ${response.statusText}`);
+
+        return response.json();
+      })
+      .then((payload: PostResponse) => {
+        if (!payload.success) throw Error(payload.message);
+
+        if (!isMisdemeanourKind(form.reason))
+          throw Error(`Reason "${form.reason}" is invalid`);
+
+        if (!payload.justTalked) {
+          const misdemeanour = createMisdemeanour(form.reason);
+
+          context.addMisdemeanour(misdemeanour);
+
+          context.changeFilter(form.reason);
+        }
+
+        navigate("/misdemeanours");
+      })
+      .catch((error) => {
+        const message =
+          error instanceof Error
+            ? error.message
+            : "Oops... Unable to save misdemeanour";
+        setErrorMessage(message);
+      });
   };
 
   return (
@@ -26,9 +74,24 @@ function ConfessionPage() {
         </p>
 
         <ConfessionForm onConfess={handleConfession} />
+
+        {errorMessage && <p className={styles.message}>{errorMessage}</p>}
       </main>
     </>
   );
+}
+
+function createMisdemeanour(reason: MisdemeanourKind) {
+  return {
+    id: crypto.randomUUID(),
+    type: reason,
+    citizenId: Math.floor(100 * Math.random()),
+    date: new Date(),
+  } as Misdemeanour;
+}
+
+function isMisdemeanourKind(value: string): value is MisdemeanourKind {
+  return MISDEMEANOURS.some((m) => m === value);
 }
 
 export { ConfessionPage };
