0174447c592a5946975596559f0144e0dcf0430e
Paul <paulwharin@gmail.com>
2024-01-15 17:05:18 +0000

    feat: add Total Misdemeanour count to Home page

--- client/src/components/home_page/home_page.tsx
+++ client/src/components/home_page/home_page.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import styles from "./home_page.module.css";
+import { MisdemeanoursContext } from "../../hooks/misdemeanour_context/misdemeanour_context";
 
 function HomePage() {
   return (
@@ -10,8 +12,25 @@ function HomePage() {
           Here you can browse a list of recent misdemeanours committed by our
           citizens, or you can confess to your own misdemeanour.
         </p>
+
+        <TotalMisdemeanours />
       </main>
     </>
+  );
+}
+
+function TotalMisdemeanours() {
+  const context = useContext(MisdemeanoursContext);
+
+  if (context.isLoading) {
+    return null;
+  }
+
+  return (
+    <div className={styles.stats}>
+      <p className={styles.name}>Total Misdemeanours:</p>
+      <p className={styles.stat}>{context.totalMisdemeanours}</p>
+    </div>
   );
 }
 
