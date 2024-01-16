65605aa1ed15e610517e2b57f04f12969c41e46b
Paul <paulwharin@gmail.com>
2024-01-16 14:33:15 +0000

    chore: remove import DummyView comment from router.tsx

--- client/src/components/router/router.tsx
+++ client/src/components/router/router.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "../layouts/main_layout";
-// import { DummyPage } from "../dummy_page/dummy_page";
 import { HomePage } from "../home_page/home_page";
 import { MisdemeanoursPage } from "../misdemeanours_page/misdemeanours_page";
 import { ConfessionPage } from "../confession_page/confession_page";
